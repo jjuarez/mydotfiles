bbcbf394ee9b34f56a98840f245d03ca4c5544a6
Javier Juarez <javier.juarez@gmail.com>
2010-08-14 00:44:54 +0200

    Fixed a little problem with symbolic links

--- .install.sh
+++ .install.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-do_link() {
 
-  ln -snf ${2} ${1}
+do_slink() {
+
+  [ -f "${1}" -o -d "${1}" ] && ln -snf ${1} ${2}
 }
 
-[ -d ~/etc/mydotfiles ] && DOTFILES="~/etc/mydotfiles" || DOTFILES=`pwd`
+
+MYDOTFILES=${MYDOTFILES:-`pwd`}
+cd
 
 ##
 # Shell
-do_link ~/.inputrc      ${DOTFILES}/.inputrc
-do_link ~/.bashrc       ${DOTFILES}/.bashrc
-do_link ~/.bash_profile ${DOTFILES}/.bash_profile
-do_link ~/.bash_logout  ${DOTFILES}/.bash_logout
+do_slink ${MYDOTFILES}/.inputrc
+do_slink ${MYDOTFILES}/.bashrc
+do_slink ${MYDOTFILES}/.bash_profile
+do_slink ${MYDOTFILES}/.bash_logout
 
 ##
 # Vim
-do_link ~/.vim          ${DOTFILES}/.vim
-do_link ~/.vimrc        ${DOTFILES}/.vim/.vimrc
-do_link ~/.gvimrc       ${DOTFILES}/.vim/.gvimrc
+do_slink ${MYDOTFILES}/.vim
+do_slink ${MYDOTFILES}/.vim/.vimrc
+do_slink ${MYDOTFILES}/.vim/.gvimrc
 
 ##
 # Ruby stuff
-do_link ~/.irbrc        ${DOTFILES}/ruby/.irbrc
-do_link ~/.gemrc        ${DOTFILES}/ruby/.gemrc
+do_slink ${MYDOTFILES}/ruby/.irbrc
+do_slink ${MYDOTFILES}/ruby/.gemrc
 
 ##
 # Screen
-do_link ~/.screenrc     ${DOTFILES}/.screenrc
+do_slink ${MYDOTFILES}/.screenrc
 
 ##
-## SSH
-do_link ~/.ssh/config   ${DOTFILES}/ssh/config
+# SSH
+do_slink ${MYDOTFILES}/ssh/config ${HOME}/.ssh/config
 
 ##
 # Git
-do_link ~/.gitconfig    ${DOTFILES}/.gitconfig
+do_slink ${MYDOTFILES}/.gitconfig
+
