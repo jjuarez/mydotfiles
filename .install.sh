e670414e5bd4b7d68f7dfaf132114d82d402ccda
Javier Juarez <jjuarez@pc8341.local>
2010-08-13 17:25:07 +0200

    Some formal changes

--- .install.sh
+++ .install.sh
@@ -7,28 +7,27 @@ do_link() {
 
 [ -d ~/etc/mydotfiles ] && DOTFILES="~/etc/mydotfiles" || DOTFILES=`pwd`
 
-cd
-
 ##
 # Shell
-do_link .inputrc      ${DOTFILES}/.inputrc
-do_link .bashrc       ${DOTFILES}/.bashrc
-do_link .bash_profile ${DOTFILES}/.bash_profile
-do_link .bash_logout  ${DOTFILES}/.bash_logout
+do_link ~/.inputrc      ${DOTFILES}/.inputrc
+do_link ~/.bashrc       ${DOTFILES}/.bashrc
+do_link ~/.bash_profile ${DOTFILES}/.bash_profile
+do_link ~/.bash_logout  ${DOTFILES}/.bash_logout
 ##
 # Vim
-do_link .vim          ${DOTFILES}/.vim
-do_link .vimrc        ${DOTFILES}/.vimrc
+do_link ~/.vim          ${DOTFILES}/.vim
+do_link ~/.vimrc        ${DOTFILES}/.vim/.vimrc
+do_link ~/.gvimrc       ${DOTFILES}/.vim/.gvimrc
 ##
 # Ruby stuff
-do_link .irbrc        ${DOTFILES}/irbrc
-do_link .rdebugrc     ${DOTFILES}/.rdebugrc
+do_link ~/.irbrc        ${DOTFILES}/ruby/.irbrc
+do_link ~/.gemrc        ${DOTFILES}/ruby/.gemrc
 ##
 # Screen
-do_link .screenrc     ${DOTFILES}/.screenrc
+do_link ~/.screenrc     ${DOTFILES}/.screenrc
 ##
 ## SSH
-do_link .ssh/config   ${DOTFILES}/ssh/config
+do_link ~/.ssh/config   ${DOTFILES}/ssh/config
 ##
 # Git
-do_link .gitconfig    ${DOTFILES}/.gitconfig
+do_link ~/.gitconfig    ${DOTFILES}/.gitconfig
