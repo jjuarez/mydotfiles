75229b4e1f1ec8d8a4d0b361e410ca22bdf75b79
Javier Juarez <javier.juarez@gmail.com>
2014-11-15 00:37:55 +0100

    Dependencia del load de la configuracion

--- lib/tasks/mydotfiles/ohmyzshtasks.rb
+++ lib/tasks/mydotfiles/ohmyzshtasks.rb
@@ -1,7 +1,7 @@
 namespace :ohmyzsh do
 
   desc "Install oh-my-zsh"
-  task :install do
+  task :install =>:load do
     begin
       omzd = File.join(ENV['HOME'], '.oh-my-zsh')
 
@@ -12,11 +12,11 @@ namespace :ohmyzsh do
   end
 
   desc "Install oh-my-zsh custom plugins"
-  task :custom_plugins do
+  task :custom_plugins =>:load do
     begin
       omzcpd = File.join(ENV['HOME'], '.oh-my-zsh', 'custom', 'plugins')
 
-      $ohmyzsh_remote_custom_plugins.each do |p|
+      $ohmyzsh_rcp.each do |p|
 
         system("git clone #{p[:repository]} #{omzcpd}/#{p[:name]}") unless File.directory?(omzcpd)
       end
