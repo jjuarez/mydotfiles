215587d780d95c02219965d83e0a402f67f5bb82
Javier Juarez <javier.juarez@tuenti.com>
2015-01-09 10:23:15 +0100

    Ahora el plugin con el PATH extendido y un alias para verificar la carga del plugin

--- shell/zsh/plugins/tuenti/tuenti.plugin.zsh
+++ shell/zsh/plugins/tuenti/tuenti.plugin.zsh
@@ -1,1 +1,15 @@
-alias foo='echo "Foo"'
+alias tuenti='echo "Yeah! your favourite plugin has loaded"'
+
+##
+# Extra path
+PATH=${PATH}:/srv/scripts/tools
+
+##
+# Make a VNC tunnel with the guest vm through gen01 host
+vnc_tunnel( ) {
+
+  local vnc_port=${1:-"5900"}
+  local jump_host=${2:-"gen01.tuenti.int"}
+
+  ssh -L ${vnc_port}:localhost:${vnc_port} ${jump_host}
+}
