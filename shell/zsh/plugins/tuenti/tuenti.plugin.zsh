9ee9030e33137059948f834b711efd9fcc7e7241
Javier Juarez <javier.juarez@tuenti.com>
2015-01-09 12:17:12 +0100

    Problema con la definicion de la function corregido

--- shell/zsh/plugins/tuenti/tuenti.plugin.zsh
+++ shell/zsh/plugins/tuenti/tuenti.plugin.zsh
@@ -6,7 +6,7 @@ PATH=${PATH}:/srv/scripts/tools
 
 ##
 # Make a VNC tunnel with the guest vm through gen01 host
-vnc_tunnel( ) {
+vnc_tunnel() {
 
   local vnc_port=${1:-"5900"}
   local jump_host=${2:-"gen01.tuenti.int"}
