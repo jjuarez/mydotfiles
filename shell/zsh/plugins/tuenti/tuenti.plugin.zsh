f50d7e43b766d253d77879fb7de90f7fa2298fdb
Javier Juarez <javier.juarez@tuenti.com>
2015-01-09 10:16:21 +0100

    Nueva utilidad en el plugin de tuenti para crear los tuneles SSH para acceso VNC

--- shell/zsh/plugins/tuenti/tuenti.plugin.zsh
+++ shell/zsh/plugins/tuenti/tuenti.plugin.zsh
@@ -1,1 +1,11 @@
 alias foo='echo "Foo"'
+
+##
+# Make a VNC tunnel with the guest vm through gen01 host
+vnc_tunnel( ) {
+
+  local vnc_port=${1:-"5900"}
+  local jump_host=${2:-"gen01.tuenti.int"}
+
+  ssh -L ${vnc_port}:localhost:${vnc_port} ${jump_host}
+}
