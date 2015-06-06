d00726b1b42923f814eae4ebc67e554bf0c35fd8
Javier Juarez <javier.juarez@tuenti.com>
2015-06-06 11:24:32 +0200

    Nuevas utilidades para pingear y observar puertos

--- shell/zsh/plugins/tuenti/tuenti.plugin.zsh
+++ shell/zsh/plugins/tuenti/tuenti.plugin.zsh
@@ -31,14 +31,43 @@ kvm_tunnel() {
 }
 
 ##
+# 
+is_alive() {
+
+  local target_host=${1}
+  local timeout=5
+  local result=1
+
+  [[ -n "${target_host}" ]] && { ping -q -c 1 -W ${timeout} ${target_host} &>/dev/null; result=${?} } 
+
+  return ${result}
+}
+
+##
+# 
+is_port_open() {
+
+  local target_host=${1}
+  local port=${2}
+  local timeout=2
+  local result=1
+
+  [[ -n "${target_host}" ]] && { nc -z -w ${timeout} ${target_host} ${port} &>/dev/null; result=${?} } 
+
+  return ${result}
+}
+
+##
 # ssh -t bastion_host ssh destination_host
 ssh_to() {
 
-  local bastion_host="gen01.tuenti.int"
+  local bastion_host="bastion"
   local target_host=${1}
 
-  [ -n "${target_host}" ] && /usr/bin/ssh -t ${bastion_host} ssh ${target_host}
+  [ -n "${target_host}" ] && /usr/bin/ssh -t ${bastion_host} /usr/bin/ssh ${target_host} 
 }
+
+
 
 ##
 # Make a dir and change to it
