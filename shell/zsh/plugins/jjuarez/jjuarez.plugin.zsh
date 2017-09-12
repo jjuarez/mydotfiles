c040c7b81c2032808cb2c6fb29e17278dafaf02e
Javier Juarez <javier.juarez@gmail.com>
2017-09-12 10:43:42 +0200

    Better handle of errors

--- shell/zsh/plugins/jjuarez/jjuarez.plugin.zsh
+++ shell/zsh/plugins/jjuarez/jjuarez.plugin.zsh
@@ -3,7 +3,7 @@
 fs::archive() {
   local directory="${1}"
 
-  [[ -d "${directory}" ]] || return 1
+  [[ -d "${directory}" ]] || return 7
 
   tar -czf ${directory}{.tar.gz,} && rm -fr "${directory}" &>/dev/null
 }
@@ -16,34 +16,52 @@ aws::launch_instance() {
   local subnet_id="${3}"
   local instance_type=${4:-'t2.micro'}
   local key_name=${5:-'Systems'}
+  local instance_id=""
 
   [[ -n "${image_id}"  ]] || return 1
-  [[ -n "${sg_id}"     ]] || return 1
-  [[ -n "${subnet_id}" ]] || return 1
+  [[ -n "${sg_id}"     ]] || return 2
+  [[ -n "${subnet_id}" ]] || return 3
 
   instance_id=$(aws ec2 run-instances --image-id "${image_id}" --count 1 --instance-type "${instance_type}" --key-name "${key_name}" --security-group-ids "${sg_id}" --subnet-id "${subnet_id}" --associate-public-ip-address|jq '.Instances[].InstanceId'|sed -e 's/"//g')
 
-  [[ -n "${instance_id}" ]] || return 1
+  [[ -n "${instance_id}" ]] || return 4
 
   aws ec2 create-tags --resources ${instance_id} --tags Key=Name,Value="Serverspec TEST instance"
 }
 
 ##
+# AWS retrieves the IP address of the instances that belongs to an ASG
+aws::get_asg_ips() {
+ local asg_name="${1}"
+ local instances=""
+
+ [[ -n "${asg_name}" ]] || return 5
+
+ instances=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "${asg_name}"|grep InstanceId|cut -d: -f2|sed -e 's/"//g'|sed -e 's/,//g')
+
+ [[ -n "${instances}" ]] || return 6
+
+ for i in "${instances}"; do
+   aws ec2 describe-instances --instance-ids "${i}"|grep PrivateIpAddress|cut -d" " -f2|head -1|cut -d, -f1|sed -e 's/"//g'
+ done
+}
+
+##
 # Puppet
 puppet::pc() {
- local pc_directory="${HOME}/workspace/4iq/devops/cm/puppet/modules/puppet_control" 
+ local pc_directory="${HOME}/workspace/4iq/devops/cm/puppet/modules/puppet_control"
 
- [[ -d "${pc_directory}" ]] && cd "${pc_directory}" 
+ [[ -d "${pc_directory}" ]] && cd "${pc_directory}"
 }
 
 terraform::iac() {
- local iac_directory="${HOME}/workspace/4iq/devops/infrastructure/iac" 
+ local iac_directory="${HOME}/workspace/4iq/devops/infrastructure/iac"
 
- [[ -d "${iac_directory}" ]] && cd "${iac_directory}" 
+ [[ -d "${iac_directory}" ]] && cd "${iac_directory}"
 }
 
 terraform::mgmt() {
- local mgmt_directory="${HOME}/workspace/4iq/devops/infrastructure/iac_management" 
+ local mgmt_directory="${HOME}/workspace/4iq/devops/infrastructure/iac_management"
 
  [[ -d "${mgmt_directory}" ]] && cd "${mgmt_directory}" 
 }
