589ca39d16e3c9c9dd803e36173ce0adfdce2c79
JJ <javier.juarez@fon.com>
2017-12-26 14:51:35 +0100

    fshow plugin

--- shell/zsh/plugins/jjuarez/jjuarez.plugin.zsh
+++ shell/zsh/plugins/jjuarez/jjuarez.plugin.zsh
@@ -8,28 +8,6 @@ fs::archive() {
   tar -czf ${directory}{.tar.gz,} && rm -fr "${directory}" &>/dev/null
 }
 
-##
-# AWS
-aws::launch_instance() {
-  local configuration_file=${1:-"${HOME}/.aws_plugin_instance.conf"}
-
-  [[ -s "${configuration_file}" ]] || return 1
-
-  source "${configuration_file}"
-
-  [[ -n "${image_id}" ]] || return 2
-  [[ -n "${subnet_id}" ]] || return 3
-  [[ -n "${sg_id}" ]] || return 4
-  [[ -n "${instance_type}" ]] || return 5
-  [[ -n "${key_name}" ]] || return 6
-
-  instance_id=$(aws ec2 run-instances --image-id "${image_id}" --count 1 --instance-type "${instance_type}" --key-name "${key_name}" --security-group-ids "${sg_id}" --subnet-id "${subnet_id}" --associate-public-ip-address|jq '.Instances[].InstanceId'|sed -e 's/"//g')
-
-  [[ -n "${instance_id}" ]] || return 7
-
-  aws ec2 create-tags --resources ${instance_id} --tags Key=Name,Value="Serverspec TEST instance"
-}
-
 aws::list_amis() {
   local configuration_file=${1:-"${HOME}/.aws_plugin_instance.conf"}
 
@@ -39,23 +17,21 @@ aws::list_amis() {
   aws ec2 describe-images --owners ${owner} | jq '.Images[] | { name: .Name, id: .ImageId }'
 }
 
-terraform::stack() {
- local dir="${HOME}/workspace/fon/devops/infra/iac_stack"
-
- [[ -d "${dir}" ]] && cd "${dir}"
+git::fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" \
+             --bind "enter:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
 }
 
-terraform::stack_live() {
- local dir="${HOME}/workspace/fon/devops/infra/iac_live"
-
- [[ -d "${dir}" ]] && cd "${dir}"
-}
 
 ##
 # Aliases
-alias launch_instance='aws::launch_instance'
-alias list_amis='aws::list_amis'
-alias stack='terraform::stack'
-alias stack_live='terraform::live'
+alias lamis='aws::list_amis'
 alias archive='fs::archive'
+alias fshow='git::fshow'
 
