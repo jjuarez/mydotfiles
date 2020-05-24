0022b9653b36d6172c9049a4973ddda256f9788a
Javier Juarez <javier.juarez@clarity.ai>
2020-05-24 18:06:56 +0200

    Moved to tools

--- mongodb/.mongorc.js
+++
@@ -1,9 +0,0 @@
-/*
- * Better install the mongo-hacker node module
- * http://tylerbrock.github.io/mongo-hacker/
- */
-var host         = db.serverStatus().host
-var current_date = ISODate().toLocaleTimeString()
-var prompt       = function() {
-  return("[" + current_date + "] " + host + "@" + db + "$ ");
-}
