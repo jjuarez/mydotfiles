13ef54992018437c55ccf902d262fefe0beefd49
Javier Juarez <javier.juarez@clarity.ai>
2020-06-17 11:53:33 +0200

    Docker image cleanup

--- shell/zsh/.functions/docker.zsh
+++ shell/zsh/.functions/docker.zsh
@@ -3,5 +3,10 @@ docker::containers::clean() {
   docker container rm $(docker container ls -q --filter status=exited)
 }
 
+docker::images::clean() {
+  docker image rm $(docker image ls|grep none|awk '{ print $1 }')
+}
+
 # alias
 alias dcc='docker::containers::clean'
+alias dic='docker::images::clean'
