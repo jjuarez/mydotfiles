618b0a81ebb6036e36e6a4853900b3b55997cbda
Javier Juarez <javier.juarez.martinez@ibm.com>
2020-06-26 16:08:44 +0200

    Recover of the iw3 password

---
+++ shell/zsh/.functions/ibm.zsh
@@ -0,0 +1,23 @@
+# vi: set ft=zsh :
+
+# set -eux -o pipefail 
+OP_CLI=$(which op 2>/dev/null)
+OPASSWORD_IBM_DOMAIN="ibm"
+
+
+ibm::_w3i_password() {
+  local password=""
+
+  [[ -x "${OP_CLI}" ]] || return 1
+
+  if [ -z "${OP_SESSION_ibm}" ]; then
+    [[ -s "${HOME}/.1password" ]] || return 1
+    eval $(cat "${HOME}/.1password"|${OP_CLI} signin --account=${OPASSWORD_IBM_DOMAIN} 2>/dev/null)
+  fi
+
+  password="$(${OP_CLI} get item 'IBM::w3id' 2>/dev/null|jq -r '.details.fields[1].value')"
+  echo "${password}"
+  return 0
+}
+
+# ::alias::
