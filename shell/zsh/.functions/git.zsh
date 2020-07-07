7cddd3644d949929f9231a4fc7e56b8af2b8a2d5
Javier Juarez <javier.juarez.martinez@ibm.com>
2020-07-07 07:43:54 +0200

    Improves the git super log

--- shell/zsh/.functions/git.zsh
+++ shell/zsh/.functions/git.zsh
@@ -1,15 +1,16 @@
 # Iteractive log
-git::fshow() {
-  git log --graph \
-          --color=always \
-          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --preview "echo {}|grep -o '[a-f0-9]\{7\}'|head -1|xargs -I % sh -c 'git show --color=always %'" \
-             --bind "enter:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
+git::superlog() {
+  git log \
+    --graph \
+    --color=always \
+    --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --height 100% --ansi --preview-window right:75% --preview "echo {}|grep -o '[a-f0-9]\{7\}'|head -1|xargs -I % sh -c 'git show --color=always %'" \
+    --bind "enter:execute:
+      (grep -o '[a-f0-9]\{7\}' | head -1 |
+      xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+      {}
 FZF-EOF"
 }
 
 # alias
-alias gitfs='git::fshow'
+alias gitsl='git::superlog'
