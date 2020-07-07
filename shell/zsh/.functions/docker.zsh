8c384bacb5bd8db5af57c0169899b63f345e9bf7
Javier Juarez <javier.juarez.martinez@ibm.com>
2020-07-07 07:43:31 +0200

    Adds the connect funtion

--- shell/zsh/.functions/docker.zsh
+++ shell/zsh/.functions/docker.zsh
@@ -3,9 +3,27 @@ docker::containers::clean() {
   docker container rm $(docker container ls -q --filter status=exited)
 }
 
+docker::containers::connect() {
+  if docker container ls >/dev/null 2>&1; then
+    local container=$(docker container ls|awk '{ if (NR!=1) print $1 ": " $(NF) }'|fzf --height 40%)
+
+    if [ -n "${container}" ]; then
+      container_id=$(echo ${container}|awk -F ': ' '{ print $1 }')
+      docker container exec -it ${container_id} /bin/bash || docker container exec -it ${container_id} /bin/sh
+    else
+      echo "No Docker container selected"
+      return 1
+    fi
+  else
+    echo "Your Docker daemon is not running!"
+    return 2
+  fi
+}
+
 docker::images::clean() {
   docker image rm $(docker image ls|grep none|awk '{ print $1 }')
 }
+
 
 # alias
 alias dcc='docker::containers::clean'
