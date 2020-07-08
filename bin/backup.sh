515af45232831e54d2c866846e27c67c879b8889
Javier Juarez <javier.juarez.martinez@ibm.com>
2020-07-08 19:47:03 +0200

    New exclusions

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,10 +3,10 @@
 declare -r TAR="gtar" # GNU tar installed by home brew
 declare -r DATE_FORMAT="%Y%m%d%H%M"
 declare -r WORKSPACE="${HOME}/workspace"
-declare -r DESTINATION_DIRECTORY="${HOME}/Dropbox/Backups/Clarity"
+declare -r DESTINATION_DIRECTORY="${HOME}/Dropbox/Backups/IBM"
 
 DATE=$(date +${DATE_FORMAT})
-DIRECTORIES=(go src linuxacademy clarity/infrastructure clarity/documentation clarity/lab clarity/product)
+DIRECTORIES=(go src linuxacademy ibm/src ibm/infrastructure clarity/documentation)
 
 
 command::do_backup( ) {
@@ -15,7 +15,7 @@ command::do_backup( ) {
   local tar_file_name="/tmp/${directory//\//-}-${DATE}.tar.gz"
 
   [[ -d "${complete_directory}" ]] && \
-    ${TAR} -czvf ${tar_file_name} --exclude='.git' --exclude='.DS_Store' --exclude='.terraform' --exclude='.terragrunt-cache' --exclude='vendor' --exclude='node_modules' --exclude='.gradle' --exclude='*.class' --exclude='*.jar' --exclude='*.csv' --exclude='.code' --exclude='.idea' -C "${complete_directory}" . && \
+    ${TAR} -czvf ${tar_file_name} --exclude='.git' --exclude='.DS_Store' --exclude='.terraform' --exclude='.terragrunt-cache' --exclude='vendor' --exclude='pkg/mod' --exclude='node_modules' --exclude='.gradle' --exclude='*.class' --exclude='*.jar' --exclude='*.csv' --exclude='.code' --exclude='.idea' --exclude=.po --exclude=__pycache__ -C "${complete_directory}" . && \
     cp "${tar_file_name}" "${DESTINATION_DIRECTORY}/" && \
     rm -f "${tar_file_name}"
 }
