b9b3f2e31db993c98d79cc9d91690c343b7bcdb3
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2020-07-23 13:50:25 +0200

    New alias for kubectx and kubens

--- shell/.functions/k8s.zsh
+++ shell/.functions/k8s.zsh
@@ -1,4 +1,5 @@
 BREW_PREFIX=$(brew --prefix)
 
+[[ -x "${BREW_PREFIX}/bin/kubectl" ]] && alias k='kubectl'
 [[ -x "${BREW_PREFIX}/bin/kubectx" ]] && alias kx='kubectx'
-[[ -x "${BREW_PREFIX}/bin/kubens"  ]] && alias nx='kubens'
+[[ -x "${BREW_PREFIX}/bin/kubens"  ]] && alias kn='kubens'
