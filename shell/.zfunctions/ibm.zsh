676d94c06dae8f443bb84d59e3d2fcb30d59e61e
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2020-12-28 18:04:27 +0100

    Simplifies the login operation with IBMCloud

--- shell/.zfunctions/ibm.zsh
+++ shell/.zfunctions/ibm.zsh
@@ -1,18 +1,14 @@
 #set -u -o pipefail
 #set -x
-declare -r DEFAULT_IBMCLOUD_ENDPOINT="cloud.ibm.com"
-
-IBMCLOUD_ENDPOINT="${IBMCLOUD_ENDOPOINT:-${DEFAULT_IBMCLOUD_ENDPOINT}}"
-
 ibm::cloud::login() {
   [[ -n "${IBMCLOUD_API_KEY}"        ]] || return 1
   [[ -n "${IBMCLOUD_REGION}"         ]] || return 1
   [[ -n "${IBMCLOUD_RESOURCE_GROUP}" ]] || return 1
 
- #ibmcloud login -a ${IBMCLOUD_ENDPOINT_DEFAULT} -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}"
-  ibmcloud login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}"
+  ibmcloud login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q
 }
 
 # ::alias::
 alias ic='ibmcloud'
 alias icl='ibm::cloud::login'
+alias ict='ibmcloud target'
