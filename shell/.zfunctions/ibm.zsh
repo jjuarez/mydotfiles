2a3ed6c62e9b0ca21c42e343441b6f6dd7c9da2e
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-02-05 09:12:37 +0100

    Better management for the IBM Cloud sessions

--- shell/.zfunctions/ibm.zsh
+++ shell/.zfunctions/ibm.zsh
@@ -1,14 +1,31 @@
 #set -u -o pipefail
 #set -x
+IBMCLOUD_SESSION_ACTIVE="${IBMCLOUD_SESSION_ACTIVE:-'false'}"
+
 ibm::cloud::login() {
+  local icCommandStatus=0
+
   [[ -n "${IBMCLOUD_API_KEY}"        ]] || return 1
   [[ -n "${IBMCLOUD_REGION}"         ]] || return 1
   [[ -n "${IBMCLOUD_RESOURCE_GROUP}" ]] || return 1
 
-  ibmcloud login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q
+  [[ "${IBMCLOUD_SESSION_ACTIVE}" == "true" ]] && {
+    ibmcloud target
+    return 0
+  }
+
+  ibmcloud login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q &&
+  export IBMCLOUD_SESSION_ACTIVE="true"
+}
+
+ibm::cloud::logout() {
+  [[ "${IBMCLOUD_SESSION_ACTIVE}" == "true" ]] || return 1
+  ibmcloud logout -q &&
+  export IBMCLOUD_SESSION_ACTIVE="false"
 }
 
 # ::alias::
 alias ic='ibmcloud'
 alias icl='ibm::cloud::login'
+alias iclo='ibm::cloud::logout'
 alias ict='ibmcloud target'
