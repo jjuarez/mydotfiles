87a469bc704136b8b6479156e9cc77969b78e371
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-03-09 20:06:22 +0100

    Better manage of the login/logout

--- shell/.zfunctions/ibm.zsh
+++ shell/.zfunctions/ibm.zsh
@@ -1,27 +1,28 @@
 #set -u -o pipefail
 #set -x
+IBMCLOUD_CLI=$(command -v ibmcloud 2>/dev/null)
 IBMCLOUD_SESSION_ACTIVE="${IBMCLOUD_SESSION_ACTIVE:-'false'}"
 
 ibm::cloud::login() {
-  local icCommandStatus=0
+  [[ "${IBMCLOUD_SESSION_ACTIVE}" == "true" ]] && return 0
+
+  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
 
   [[ -n "${IBMCLOUD_API_KEY}"        ]] || return 1
   [[ -n "${IBMCLOUD_REGION}"         ]] || return 1
   [[ -n "${IBMCLOUD_RESOURCE_GROUP}" ]] || return 1
 
-  [[ "${IBMCLOUD_SESSION_ACTIVE}" == "true" ]] && {
-    ibmcloud target
-    return 0
-  }
-
-  ibmcloud login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q &&
-  export IBMCLOUD_SESSION_ACTIVE="true"
+  ${IBMCLOUD_CLI} login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q &&
+  export IBMCLOUD_SESSION_ACTIVE='true'
 }
 
 ibm::cloud::logout() {
-  [[ "${IBMCLOUD_SESSION_ACTIVE}" == "true" ]] || return 1
-  ibmcloud logout -q &&
-  export IBMCLOUD_SESSION_ACTIVE="false"
+  [[ "${IBMCLOUD_SESSION_ACTIVE}" == 'true' ]] || return 0
+
+  [[ -n "${IBMCLOUD_CLI}" ]] || return 1
+
+  ${IBMCLOUD_CLI} logout -q &&
+  export IBMCLOUD_SESSION_ACTIVE='false'
 }
 
 # ::alias::
