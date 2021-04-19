be88a8c0ec1d98afd657f3b9ff316e8d98c7f24f
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-04-19 12:15:58 +0200

    Some identation issues

--- shell/.zfunctions/k8s.zsh
+++ shell/.zfunctions/k8s.zsh
@@ -3,6 +3,5 @@
 
 HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-$(brew --prefix)}
 
-[[ -x "${HOMEBREW_PREFIX}/bin/kubectl" ]] && alias k='kubectl'
+[[ -x "${HOMEBREW_PREFIX}/bin/kubectl"   ]] && alias k='kubectl'
 [[ -x "${HOMEBREW_PREFIX}/bin/kubecolor" ]] && alias k='kubecolor'
-
