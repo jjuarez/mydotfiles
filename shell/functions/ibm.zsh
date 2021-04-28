9b0edf1d7759a2bcfb5b83f181395e87c1d5caa9
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-04-28 11:26:14 +0200

    A few improvements over the ibm cloud functions

--- shell/functions/ibm.zsh
+++ shell/functions/ibm.zsh
@@ -9,22 +9,25 @@ ibm::cloud::login() {
   [[ -n "${IBMCLOUD_REGION}"         ]] || return 1
   [[ -n "${IBMCLOUD_RESOURCE_GROUP}" ]] || echo -e "Warning: No IBMCloud resource group specified"
 
-  ${IBMCLOUD_CLI} login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q &&
-  export IBMCLOUD_SESSION_ACTIVE='true'
+  ${IBMCLOUD_CLI} login -r ${IBMCLOUD_REGION} -g "${IBMCLOUD_RESOURCE_GROUP}" -q >/dev/null 2>&1
 }
 
 ibm::cloud::logout() {
   [[ -x "${IBMCLOUD_CLI}" ]] || return 1
 
-  ${IBMCLOUD_CLI} logout -q
+  ${IBMCLOUD_CLI} logout -q >/dev/null 2>&1
 }
 
 ibm::k8s::ls() {
-  ${IBMCLOUD_CLI} ks cluster ls -q
+  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
+
+  ${IBMCLOUD_CLI} ks cluster ls -q 2>/dev/null
 }
 
 ibm::k8s::gakc() {
   local resource_group_id_prev=""
+
+  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
 
   for cluster_info in $(${IBMCLOUD_CLI} ks cluster ls --output json -q|jq 'sort_by(.resourceGroup)'|jq '.[]|"\(.resourceGroup),\(.name)"'|tr -d '"'); do
     local resource_group_id=$(echo "${cluster_info}"|awk -F, '{ print $1 }')
@@ -38,7 +41,7 @@ ibm::k8s::gakc() {
     ${IBMCLOUD_CLI} ks cluster config --cluster "${cluster_name}" -q &&
       cp -f "${HOME}/.kube/config" "${HOME}/.kube/${cluster_name}.yml" &&
       rm -f "${HOME}/.kube/config" &&
-      echo -e "k8s cluster configuration for: ${cluster_name} updated!"
+      echo -e "Cluster configuration ${cluster_name} updated!"
   done
 }
 
@@ -53,3 +56,4 @@ alias ic='ibmcloud'
 alias icli='ibm::cloud::login'
 alias iclo='ibm::cloud::logout'
 alias ict='ibmcloud target'
+alias kls='ibm::k8s::ls'
