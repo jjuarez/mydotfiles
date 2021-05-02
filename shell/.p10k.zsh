557a312bcf411543b1eabb5ed652354a11eafc5a
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-05-02 11:27:29 +0200

    New name to configure powerlevel9k prompt

--- shell/.p10k.zsh
+++ shell/.p10k.zsh
@@ -1,9 +1,9 @@
-[[ "${ZSH_THEME}" == "powerlevel10k" ]] && {
-  POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
-  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir nodenv pyenv kubecontext vcs direnv)
+if [[ "${ZSH_THEME}" == "powerlevel10k" ]]; then
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(wifi dir nodenv pyenv kubecontext vcs direnv)
   POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
   POWERLEVEL9K_MODE="nerdfont-complete"
   POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
   POWERLEVEL9K_SHORTEN_DELIMITER="·"
   POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_left"
-}
+  POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|k9s|stern|kubie'
+fi
