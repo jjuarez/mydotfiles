be6f3d97ba70c88674f46dee40ea535fe15bc16c
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-05-03 21:19:14 +0200

    Deletes some options to do no lose the k8s prompt

--- shell/.p10k.zsh
+++ shell/.p10k.zsh
@@ -5,5 +5,4 @@ if [[ "${ZSH_THEME}" == "powerlevel10k" ]]; then
   POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
   POWERLEVEL9K_SHORTEN_DELIMITER="·"
   POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_left"
-  POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|k9s|stern|kubie'
 fi
