8632bfe9ef8e393e004c89657253261811d9dd88
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-05-03 17:17:46 +0200

    Support for templates

---
+++ tools/.skeletons/README.md
@@ -0,0 +1,5 @@
+# Title
+
+## Subtitle
+
+* Bullet points
---
+++ tools/.skeletons/bash.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+#set -x
+
+declare -r DEFAULT_FOO="Foo value"
+
+FOO="${FOO:-${DEFAULT_FOO}}"
+
+utils::console() {
+  local -r message="${1}"
+
+  [[ -n "${message}" ]] && echo -e "${message}"
+}
+
+utils::die() {
+  local -r message="${1}"
+  local -ri exit_code="${2}"
+
+  utils::console "${message}"
+  exit "${exit_code}"
+}
+
+#
+# ::main::
+#
+utils::console "Your code goes here!"
