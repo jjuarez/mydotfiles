63e2bc00d2589823e12c9792d996a2b6077d4b52
Francisco Javier Juarez Martinez <Javier.Juarez.Martinez@ibm.com>
2021-05-28 12:07:53 +0200

    Adds the multisource function

--- shell/zsh.d/utils.zsh
+++ shell/zsh.d/utils.zsh
@@ -1,1 +1,14 @@
+utils::multi_source() {
+  local -r files="${@}"
+
+  for f in $(echo ${files}|tr ' ' '\n'); do
+    [[ -f "${f}" ]] && source "${f}"
+  done
+}
+
+# autoloads
+autoload utils::multi_source
+
+# aliases
+alias msource='utils::multi_source'
 alias jsontidy='pbpaste|jq '.'|pbcopy'
