78bc3a5352dde77b8cd1df0b1796e4ae8d07044a
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-12-16 19:55:17 +0100

    No k8s specific aliases, these are only aliases

--- shell/zsh.d/utils.zsh
+++ shell/zsh.d/utils.zsh
@@ -12,3 +12,8 @@ autoload utils::multi_source
 # aliases
 alias msource='utils::multi_source'
 alias jsontidy='pbpaste|jq '.'|pbcopy'
+
+HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-$(brew --prefix)}
+
+[[ -x "${HOMEBREW_PREFIX}/bin/kubectl"   ]] && alias k='kubectl'
+[[ -x "${HOMEBREW_PREFIX}/bin/kubecolor" ]] && alias k='kubecolor'
