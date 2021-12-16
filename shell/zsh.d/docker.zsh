bdff872ff01d575c6895f911de6b47238dec9cb7
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-12-16 21:02:18 +0100

    Looked over the node versions and the artifactory images

--- shell/zsh.d/docker.zsh
+++ shell/zsh.d/docker.zsh
@@ -1,16 +1,26 @@
 #set -u -o pipefail
 #set -x
 declare NODE_DOCKER_IMAGES=(
-  14.17.6-alpine3.14
-  16.10.0-alpine3.14
-  16.11.0-alpine3.14
+  14.18.2-alpine3.14
+  16.13.1-alpine3.14
+  17.2.0-alpine3.14
 )
 declare ARTIFACTORY_DOCKER_REGISTRY="res-quantum-ci-docker-virtual.artifactory.swg-devops.com"
 declare ARTIFACTORY_DOCKER_IMAGES=(
+  postgres:11-alpine
   postgres:12.8-alpine3.14
+  postgres:12.9-alpine3.14
+  mongo:4.4.6
+  nats:2.1.9-alpine
   nats:2.5.0-alpine3.14
+  nats:2.6.6-alpine3.14
   minio/minio:RELEASE.2021-02-14T04-01-33Z
+  minio/minio:RELEASE.2021-11-24T23-19-33Z
+  redis:6.0.8-alpine
   redis:6.0.15-alpine3.14
+  redis:6.0.16-alpine3.14
+  segment/mock:v0.0.2
+  busybox:latest
 )
 
 # Docker clean exited images
@@ -34,13 +44,13 @@ for di in $(docker image list --format "{{.Repository}} {{.Tag}}"|grep latest|gr
 docker::images::update_node() {
   for ni in "${NODE_DOCKER_IMAGES[@]}"; do
     echo "Docker image update: ${ni}..."
-    docker image pull "node:${ni}"
+    docker image pull "node:${ni}" 2>/dev/null
   done
 }
 
 docker::images::artifactory_clean() {
   for di in "${ARTIFACTORY_DOCKER_IMAGES[@]}"; do
-    docker image rm ${ARTIFACTORY_DOCKER_REGISTRY}/${di}
+    docker image rm -f ${ARTIFACTORY_DOCKER_REGISTRY}/${di} 2>/dev/null
   done
 }
 
