ff136adaf3311c2985d32773794bc649dc940d82
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-12-16 21:01:52 +0100

    Adds some alias related to kubectl

--- shell/zsh.d/utils.zsh
+++ shell/zsh.d/utils.zsh
@@ -1,3 +1,5 @@
+HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-$(brew --prefix)}
+
 utils::multi_source() {
   local -r files="${@}"
 
@@ -13,7 +15,5 @@ autoload utils::multi_source
 alias msource='utils::multi_source'
 alias jsontidy='pbpaste|jq '.'|pbcopy'
 
-HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-$(brew --prefix)}
-
 [[ -x "${HOMEBREW_PREFIX}/bin/kubectl"   ]] && alias k='kubectl'
-[[ -x "${HOMEBREW_PREFIX}/bin/kubecolor" ]] && alias k='kubecolor'
+[[ -x "${HOMEBREW_PREFIX}/bin/kubecolor" ]] && alias k='kubecolor' # Override
