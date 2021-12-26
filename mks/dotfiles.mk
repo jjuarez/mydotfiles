6a140a58a4eb30489da56fa324bdc656cf6272e2
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-12-26 20:57:03 +0100

    Dotfiles support with make

---
+++ mks/dotfiles.mk
@@ -0,0 +1,16 @@
+FILES := $(shell yq e '.dotfiles' $(DOTFILES)/config.yml|sed -e 's/\- //g')
+
+
+PHONY: dotfiles/install
+dotfiles/install: ## Install dotfiles
+	@for f in $(FILES); do \
+		echo "Linking $${DOTFILES}/$${f} $${HOME}/`basename $${f}`" ; \
+		ln -nfs $${DOTFILES}/$${f} $${HOME}/`basename $${f}` ; \
+	done \
+
+PHONY: dotfiles/uninstall
+dotfiles/uninstall: ## Install dotfiles
+	@for f in $(FILES); do \
+		echo "Deleting: $${HOME}/`basename $${f}`" ; \
+		rm -f $${HOME}/`basename $${f}` ; \
+	done \
