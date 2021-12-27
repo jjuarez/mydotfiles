63aa70bc281fb8a83281abc6f93fb1f472319eab
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-12-27 10:46:57 +0100

    Makefile linter issues

--- mks/dotfiles.mk
+++ mks/dotfiles.mk
@@ -1,14 +1,14 @@
 FILES := $(shell yq e '.dotfiles' $(CONFIG_FILE)|sed -e 's/\- //g')
 
 
-PHONY: dotfiles/install
+.PHONY: dotfiles/install
 dotfiles/install: ## Install dotfiles
 	@for f in $(FILES); do \
 		echo "Linking $${DOTFILES}/$${f} $${HOME}/`basename $${f}`" ; \
 		ln -nfs $${DOTFILES}/$${f} $${HOME}/`basename $${f}` ; \
 	done \
 
-PHONY: dotfiles/uninstall
+.PHONY: dotfiles/uninstall
 dotfiles/uninstall: ## Uninstall dotfiles
 	@for f in $(FILES); do \
 		echo "Deleting: $${HOME}/`basename $${f}`" ; \
--- mks/ssh.mk
+++ mks/ssh.mk
@@ -1,8 +1,8 @@
-PHONY: ssh/install
+.PHONY: ssh/install
 ssh/install: ## SSH install custom configuration
 	@test -f $(DOTFILES)/tools/ssh/config && \
 	cp $(DOTFILES)/tools/ssh/config $(HOME)/.ssh/config 2>/dev/null
 
-PHONY: ssh/uninstall
+.PHONY: ssh/uninstall
 ssh/uninstall: ## SSH uninstall custom configuration
 	@test -f $(HOME)/.ssh/config && rm -f $(HOME)/.ssh/config
