77c674eaa43c5395fa83d511fb97a262d7157eb7
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-12-28 09:35:17 +0100

    Add the stuff related to the theme must be here

--- shell/.p10k.zsh
+++ shell/.p10k.zsh
@@ -5,4 +5,8 @@ if [[ "${ZSH_THEME}" == "powerlevel10k" ]]; then
   POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
   POWERLEVEL9K_SHORTEN_DELIMITER="·"
   POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_left"
+
+  if [[ -r "${XDG_CACHE_HOME:-${HOME}/.cache}/p10k-instant-prompt-${(%)}:-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-${HOME}/.cache}/p10k-instant-prompt-${(%)}:-%n}.zsh"
+  fi
 fi
