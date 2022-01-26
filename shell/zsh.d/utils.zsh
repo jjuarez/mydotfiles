61c72c3d44ec4f3ccbfa69a0bc68e1278da8f5fc
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-01-26 10:38:31 +0100

    Cleanup

--- shell/zsh.d/utils.zsh
+++ shell/zsh.d/utils.zsh
@@ -1,18 +1,8 @@
 HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-$(brew --prefix)}
 
-utils::multi_source() {
-  local -r files="${@}"
-
-  for f in $(echo ${files}|tr ' ' '\n'); do
-    [[ -f "${f}" ]] && source "${f}"
-  done
-}
-
 # autoloads
-autoload utils::multi_source
 
 # aliases
-alias msource='utils::multi_source'
 alias jsontidy='pbpaste|jq '.'|pbcopy'
 
 [[ -x "${HOMEBREW_PREFIX}/bin/kubectl"   ]] && alias k='kubectl'
