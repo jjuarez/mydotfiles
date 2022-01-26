1ac68993ad8fb3508d89aa2b35f0129f72d8754f
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-01-26 11:04:34 +0100

    Better management of the keys

--- shell/zsh.d/ssh-agent.zsh
+++ shell/zsh.d/ssh-agent.zsh
@@ -1,8 +1,10 @@
 ssh::load_keys() {
+  local -a zssh_ids
+
   zstyle -a ':zim:ssh' ids 'zssh_ids'
 
   if (( ${#zssh_ids} )); then
-    ssh-add -q ${HOME}/.ssh/${^zssh_ids}
+    ssh-add -q ${HOME}/.ssh/${^zssh_ids} 2>/dev/null
   else
     ssh-add 2>/dev/null
   fi
