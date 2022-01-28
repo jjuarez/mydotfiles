e6b49cbda45ac406ea2455c62fdcd2f080512f2a
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-01-28 11:52:35 +0100

    Checkpoint from Make to ansible

---
+++ roles/dotfiles/files/shell/zsh.d/p10k.zsh
@@ -0,0 +1,12 @@
+if [[ "${ZSH_THEME}" == "powerlevel10k" ]]; then
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir nodenv pyenv virtualenv kubecontext vcs)
+  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
+  POWERLEVEL9K_MODE="nerdfont-complete"
+  POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+  POWERLEVEL9K_SHORTEN_DELIMITER="·"
+  POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_left"
+
+  if [[ -r "${XDG_CACHE_HOME:-${HOME}/.cache}/p10k-instant-prompt-${(%)}:-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-${HOME}/.cache}/p10k-instant-prompt-${(%)}:-%n}.zsh"
+  fi
+fi
