e069d2385311d2d9db432393fd25ecaa07a373a2
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-03-07 17:02:29 +0100

    Packed all the aliases

--- roles/dotfiles/files/shell/zsh.d/utils.zsh
+++
@@ -1,9 +0,0 @@
-HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-$(brew --prefix)}
-
-# autoloads
-
-# aliases
-alias jsontidy='pbpaste|jq '.'|pbcopy'
-
-[[ -x "${HOMEBREW_PREFIX}/bin/kubectl"   ]] && alias k='kubectl'
-[[ -x "${HOMEBREW_PREFIX}/bin/kubecolor" ]] && alias k='kubecolor' # Override
