018a0cdc9b6cd2ca756aeb5c993e35602063f536
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-04-07 22:49:20 +0200

    feat: Better way to manage clean and virtual environment activation/deactivation

--- roles/dotfiles/files/shell/zsh.d/python.zsh
+++ roles/dotfiles/files/shell/zsh.d/python.zsh
@@ -3,8 +3,15 @@
 
 
 python::clean() {
-  find . -type f -name "*.py[co]" -delete
-  find . -type d -name "__pycache__" -delete
+  find . -type f -iname "*.py[co]" -delete
+  find . -type d -iname "__pycache__" -delete
+}
+
+python::venv::activate() {
+  local venv_directory="./venv"
+
+  [[ -d ./.venv ]] && venv_directory="./.venv"
+  deactivate &>/dev/null; source "${venv_directory}/bin/activate"
 }
 
 # autoloads
@@ -12,3 +19,5 @@ autoload python::clean
 
 # aliases
 alias pyclean='python::clean'
+alias ae='python::venv::activate'
+alias de='deactivate'
