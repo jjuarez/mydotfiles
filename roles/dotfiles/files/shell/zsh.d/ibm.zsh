f9a2ca71795557dcae03df97315752e9fe3abdf9
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2023-02-02 22:04:54 +0100

    feat: New version to use with SSO login and multiple accounts

--- roles/dotfiles/files/shell/zsh.d/ibm.zsh
+++ roles/dotfiles/files/shell/zsh.d/ibm.zsh
@@ -1,98 +1,88 @@
-#set -u -o pipefail
+set -o pipefail
 #set -x
 
-# IBMCLOUD_DEBUG="true"
 IBMCLOUD_CLI=$(command -v ibmcloud 2>/dev/null)
 IKSCC=$(command -v ikscc 2>/dev/null)
 
-typeset -A IBM_CLUSTERS
+typeset -A IBM_ACCOUNTS=(
+  ["qc-master"]="3f0eacee15cc4551a4b51313a4a1f2d2"
+  ["qs-staging"]="f3e7d1b7a7044d7abf45f5be9821782a"
+  ["qs-production"]="b947c1c5e9344d64aed96696e4d76e0e"
+)
+typeset -A IBM_CLUSTERS=(
+# Quantum Master
+  [apis-dev]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [apis-prod]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [apis-dev-de]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [apis-prod-de]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [apps-staging-de]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [apps-staging-us]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [apps-prod-us]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [processing-staging]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [processing-staging-de]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [processing-prod]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [processing-production-de]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [experimental-us]="3f0eacee15cc4551a4b51313a4a1f2d2|iks"
+  [sat-ykt-openq-dev]="3f0eacee15cc4551a4b51313a4a1f2d2|openshift"
+  [sat-pok-qnet-prod]="3f0eacee15cc4551a4b51313a4a1f2d2|openshift"
+  [sat-pok-qnet-staging]="3f0eacee15cc4551a4b51313a4a1f2d2|openshift"
+  [cicd-production]="3f0eacee15cc4551a4b51313a4a1f2d2|openshift"
+  [cicd-tools]="3f0eacee15cc4551a4b51313a4a1f2d2|openshift"
+# Quantum Services - staging
+  [qc-apis-staging-us-east]="f3e7d1b7a7044d7abf45f5be9821782a|iks"
+  [qc-apis-testing-us-east]="f3e7d1b7a7044d7abf45f5be9821782a|iks"
+  [qc-pok-qnet-staging]="f3e7d1b7a7044d7abf45f5be9821782a|openshift"
+  [qc-simulators-staging-us-east]="f3e7d1b7a7044d7abf45f5be9821782a|openshift"
+# Quantum Services - production
+  [sat-ccf-prod]="b947c1c5e9344d64aed96696e4d76e0e|openshift"
+)
 
 
-ibm::cloud::load_cluster_configs() {
-# IBM_CLUSTERS[apis-dev]="Clusters Non-Prod|iks"
-# IBM_CLUSTERS[apis-prod]="Clusters|iks"
-# IBM_CLUSTERS[apis-dev-de]="Clusters Non-Prod - DE|iks"
-# IBM_CLUSTERS[apis-prod-de]="Clusters - DE|iks"
-# IBM_CLUSTERS[apps-staging-de]="Clusters Non-Prod - DE|iks"
-# IBM_CLUSTERS[apps-staging-us]="Clusters Non-Prod|iks"
-# IBM_CLUSTERS[apps-prod-us]="Clusters|iks"
-# IBM_CLUSTERS[processing-staging]="Clusters Non-Prod|iks"
-# IBM_CLUSTERS[processing-staging-de]="Clusters Non-Prod - DE|iks"
-# IBM_CLUSTERS[processing-prod]="Clusters|iks"
-# IBM_CLUSTERS[processing-production-de]="Clusters - DE|iks"
-# IBM_CLUSTERS[experimental-us]="Experimental|iks"
-  IBM_CLUSTERS[sat-ykt-openq-dev]="IBM Satellite Clusters Non-Prod|openshift"
-  IBM_CLUSTERS[sat-pok-qnet-prod]="IBM Satellite Clusters|openshift"
-  IBM_CLUSTERS[sat-pok-qnet-staging]="IBM Satellite Clusters Non-Prod|openshift"
-  IBM_CLUSTERS[cicd-production]="Infrastructure Core|openshift"
-  IBM_CLUSTERS[cicd-tools]="CI-CD Services|openshift"
-}
+ibm::cloud::switch_account() {
+  local -r account_id="${1}"
 
-ibm::cloud::login() {
-  [[ -x "${IBMCLOUD_CLI}"     ]] || return 1
-  [[ -n "${IBMCLOUD_API_KEY}" ]] || return 2
-  [[ -n "${IBMCLOUD_REGION}"  ]] || IBMCLOUD_REGION='us-south'
-
-  ${IBMCLOUD_CLI} login -r ${IBMCLOUD_REGION} -q >/dev/null 2>&1
-}
-
-ibm::cloud::logout() {
+  [[ -n "${account_id}" ]] || return 1
   [[ -x "${IBMCLOUD_CLI}" ]] || return 1
 
-  ${IBMCLOUD_CLI} logout -q >/dev/null 2>&1
-}
-
-ibm::cloud::target() {
-  local -r resource_group=${1:-''}
-  local -r region=${2:-''}
-
-  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
-
-  ${IBMCLOUD_CLI} target -g "${resource_group}" -r "${region}" -q >/dev/null
+  case ${IBMCLOUD_DEBUG} in
+    true) ${IBMCLOUD_CLI} target -c "${account_id}" ;;
+       *) ${IBMCLOUD_CLI} target -c "${account_id}" -q >/dev/null 2>&1 ;;
+  esac
 }
 
 ibm::k8s::update() {
-  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
-  [[ -x "${IKSCC}"        ]] || return 1
+  local current_account=""
 
-  ibm::cloud::target
-  ibm::cloud::load_cluster_configs
+  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
+  [[ -x "${IKSCC}" ]] || return 1
+
+  ${IBMCLOUD_CLI} target -g '' -r '' -q >/dev/null 2>&1
 
   for cluster data in ${(kv)IBM_CLUSTERS}; do
+    local account=$(echo ${data}|awk -F"|" '{ print $1 }')
     local kind=$(echo ${data}|awk -F"|" '{ print $2 }')
 
-    echo "${cluster}"
+    if [[ -z "${current_account}" ]]; then
+      ibm::cloud::switch_account "${account}" &&
+      current_account="${account}"
+    elif [[ "${current_account}" != "${account}" ]]; then
+      ibm::cloud::switch_account "${account}" &&
+      current_account="${account}"
+    fi
+
+    echo "Cluster: ${cluster} (${account},${kind})"
+
     case ${kind} in
-      openshift) ${IBMCLOUD_CLI} ks cluster config --cluster ${cluster} --output yaml -q --admin | ${IKSCC} -f - >! "${HOME}/.kube/${cluster}.yml" || return 7 ;;
-            iks) ${IBMCLOUD_CLI} ks cluster config --cluster ${cluster} --output yaml -q | ${IKSCC} -f - >! "${HOME}/.kube/${cluster}.yml" || return 7 ;;
-              *) return 8 ;;
+      openshift) ${IBMCLOUD_CLI} ks cluster config --cluster ${cluster} --output yaml -q --admin --endpoint link | ${IKSCC} -f - >! "${HOME}/.kube/${cluster}.yml" || return 3 ;;
+            iks) ${IBMCLOUD_CLI} ks cluster config --cluster ${cluster} --output yaml -q | ${IKSCC} -f - >! "${HOME}/.kube/${cluster}.yml" || return 3 ;;
+              *) return 4 ;;
     esac
   done
 }
 
-ibm::k8s::show() {
-  [[ -x "${IBMCLOUD_CLI}" ]] || return 1
-  [[ -x "${IKSCC}"        ]] || return 1
-
-  ibm::cloud::target
-  ibm::cloud::load_cluster_configs
-
-  for cluster data in ${(kv)IBM_CLUSTERS}; do
-    local kind=$(echo ${data}|awk -F"|" '{ print $2 }')
-    echo "Cluster: ${cluster} (${kind})"
-  done
-}
-
 # autoloads
-autoload ibm::cloud::login
-autoload ibm::cloud::logout
-autoload ibm::cloud::target
-autoload ibm::k8s::update
-autoload ibm::k8s::show
+autoload ibm::cloud::switch_account
 
 # aliases
 alias ic='ibmcloud'
-alias ic.li='ibm::cloud::login'
-alias ic.lo='ibm::cloud::logout'
-alias ic.t='ibm::cloud::target'
-alias ic.ks.up='ibm::k8s::update'
+alias ic.sa='ibm::cloud::switch_account'
