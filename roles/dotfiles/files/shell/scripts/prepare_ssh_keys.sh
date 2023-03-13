56aa1f79766f4c18adde9ab9e3b667d16c910043
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2023-03-13 11:36:54 +0100

    feat: A new script to prepare the bastion SSH keys

---
+++ roles/dotfiles/files/shell/scripts/prepare_ssh_keys.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+: '
+This shell script will help you to user the SSH privates keys used in the tunnels and DTB
+
+# Usage:
+
+## Requirements
+ - Having the SSH private keys in the ${HOME}/.ssh directory
+
+'
+
+set -e -o pipefail
+
+declare -r TEMPORAL_DIRECTORY=${TEMPORAL_DIRECTORY:-'/tmp'}
+declare -A SSH_PRIVATE_KEYS=(
+  [/tmp/id_rsa.qnet]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.qnet"
+  [/tmp/id_rsa.openq]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.openq"
+  [/tmp/id_rsa.ccf]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.ccf"
+)
+
+
+main() {
+  for dst src in ${(kv)SSH_PRIVATE_KEYS}; do
+    [[ -f "${src}" ]] && cp -avf "${src}" "${dst}"
+  done
+}
+
+#
+# ::main::
+#
+main "${@}"
