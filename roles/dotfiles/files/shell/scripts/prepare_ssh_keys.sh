70aae57d0c3fd568fa76d039f039087a80a9f6b5
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2023-06-05 11:43:33 +0200

    feat: This new version of the script plays with the temp directory configuration

--- roles/dotfiles/files/shell/scripts/prepare_ssh_keys.sh
+++ roles/dotfiles/files/shell/scripts/prepare_ssh_keys.sh
@@ -12,23 +12,53 @@ This shell script will help you to user the SSH privates keys used in the tunnel
 
 set -eu -o pipefail
 
-declare -r TEMPORAL_DIRECTORY=${TEMPORAL_DIRECTORY:-'/tmp'}
+declare -r DEFAULT_TEMPORAL_DIRECTORY="/tmp"
+declare -r TEMPORAL_DIRECTORY="${TEMPORAL_DIRECTORY:-${DEFAULT_TEMPORAL_DIRECTORY}}"
 declare -A SSH_PRIVATE_KEYS=(
-  [/tmp/id_rsa.qnet]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.qnet"
-  [/tmp/id_rsa.openq]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.openq"
-  [/tmp/id_rsa.ccf]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.ccf"
-  [/tmp/id_rsa.sk]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.sk"
+  [id_rsa.qnet]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.qnet"
+  [id_rsa.openq]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.openq"
+  [id_rsa.ccf]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.ccf"
+  [id_rsa.sk]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.sk"
+  [d_rsa.bmt]="${HOME}/.ssh/id_rsa.ibm.runtimedeployusr.bmt"
 )
 
 
-main() {
-  for dst src in ${(kv)SSH_PRIVATE_KEYS}; do
-    [[ -d "${dst}" ]] && rm -fr "${dst}"
-    [[ -f "${src}" ]] && cp -avf "${src}" "${dst}"
+check() {
+  for ssh_key in ${(k)SSH_PRIVATE_KEYS}; do
+    local dst_filename="${TEMPORAL_DIRECTORY}/${ssh_key}"
+
+    [[ ! -f "${dst_filename}" ]] && echo "There's no ${dst_filename}"
   done
 }
+
+
+clean() {
+  for ssh_key in ${(k)SSH_PRIVATE_KEYS}; do
+    local dst_filename="${TEMPORAL_DIRECTORY}/${ssh_key}"
+
+    [[ -f "${dst_filename}" ]] && rm -fr "${dst_filename}"
+  done
+}
+
+
+install() {
+  for ssh_key src in ${(kv)SSH_PRIVATE_KEYS}; do
+    local dst_filename="${TEMPORAL_DIRECTORY}/${ssh_key}"
+
+    [[ -d "${dst_filename}" ]] && rm -fr "${dst_filename}"
+    [[ -f "${src}" ]] && cp -avf "${src}" "${dst_filename}"
+  done
+}
+
 
 #
 # ::main::
 #
-main "${@}"
+COMMAND="${1:-'none'}"
+
+case "${COMMAND}" in
+    check) check ;;
+    clean) clean ;;
+  install) install ;;
+        *) echo -e "Usage: ${0} (install|clean)" ;;
+esac
