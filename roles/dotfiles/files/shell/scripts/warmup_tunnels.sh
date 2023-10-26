78c9ebfc4a85e1e21de9090ec7bd9749138f1861
Javier Juarez <javier.juarez@gmail.com>
2023-10-26 18:45:52 +0200

    Featureansible updates (#16)
    * chore: Cleanup
    
    * fix: Avoid to pre-load the runtime deploy user SSH key
    
    * feat: Simpler and more clear SSH client configuration
    
    * feat: New version
    
    * fix: A few linter issues
    
    ---------
    
    Co-authored-by: javier.juarez <Javier.Juarez.Martinez@ibm.com>

--- roles/dotfiles/files/shell/scripts/warmup_tunnels.sh
+++ roles/dotfiles/files/shell/scripts/warmup_tunnels.sh
@@ -1,21 +1,40 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-set -e -o pipefail
+set -eu -o pipefail
+#set -x
 
 declare -r SSH="/usr/bin/ssh"
 declare -r DEFAULT_CHECK_COMMAND="hostname"
 declare -r DEFAULT_DEBUG="false"
+
+declare -r QNET_JUMP_HOST="qnet.jumphost"
+declare -r QNET_MUX_SOCKET="/tmp/ssh_mux_qnet.sock"
+declare -r OPENQ_JUMP_HOST="openq.jumphost"
+declare -r OPENQ_MUX_SOCKET="/tmp/ssh_mux_openq.sock"
 
 # Configuration
 CHECK_COMMAND=${CHECK_COMMAND:-${DEFAULT_CHECK_COMMAND}}
 DEBUG=${DEBUG:-${DEFAULT_DEBUG}}
 
 
-utils::console() {
-  local -r message="${1}"
+activate::qnet() {
+  local log_level="${1:-'QUIET'}"
 
-  [[ -n "${message}" ]] && echo -e "${message}" >&2
+  if [[ ! -S "${QNET_MUX_SOCKET}" ]]; then
+    echo "Activating QNet jumphost with 2FA..."
+    ${SSH} -oLogLevel="${log_level}" "${QNET_JUMP_HOST}" "${CHECK_COMMAND}" || return 1
+  fi
 }
+
+activate::openq() {
+  local log_level="${1:-'QUIET'}"
+
+  if [[ ! -S "${OPENQ_MUX_SOCKET}" ]]; then
+    echo "Activating OpenQ jumphost with 2FA..."
+    ${SSH} -oLogLevel="${log_level}" "${OPENQ_JUMP_HOST}" "${CHECK_COMMAND}" || return 1
+  fi
+}
+
 
 main() {
   local log_level="QUIET"
@@ -24,11 +43,10 @@ main() {
     true) log_level="DEBUG" ;;
   esac
 
-  for jh in openq qnet; do
-    utils::console "Warmup ${jh} jumphost with 2FA..."
-    ${SSH} -oLogLevel=${log_level} ${jh}.jumphost ${CHECK_COMMAND}
-  done
+  activate::qnet ${log_level}
+  activate::openq ${log_level}
 }
+
 
 #
 # ::main::
