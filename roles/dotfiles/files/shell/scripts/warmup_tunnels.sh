bd3491139eff2e214ce30d72b9ab9e4a70c50305
JJ <javier.juarez.martinez@ibm.com>
2025-03-08 11:13:12 +0100

    feat: A small improvement in the management of errors

--- roles/dotfiles/files/shell/scripts/warmup_tunnels.sh
+++ roles/dotfiles/files/shell/scripts/warmup_tunnels.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-set -eu -o pipefail
+set -u -o pipefail
 #set -x
 
 declare -r SSH="/usr/bin/ssh"
 declare -r DEFAULT_CHECK_COMMAND="hostname"
 declare -r DEFAULT_DEBUG="false"
+declare -r DEFAULT_LOG_LEVEL="INFO"
 
 declare -r QNET_JUMP_HOST="qnet.jumphost"
 declare -r QNET_MUX_SOCKET="/tmp/ssh_mux_qnet.sock"
@@ -14,39 +15,41 @@ declare -r OPENQ_MUX_SOCKET="/tmp/ssh_mux_openq.sock"
 
 # Configuration
 CHECK_COMMAND=${CHECK_COMMAND:-${DEFAULT_CHECK_COMMAND}}
+LOG_LEVEL=${LOG_LEVEL:-${DEFAULT_LOG_LEVEL}}
 DEBUG=${DEBUG:-${DEFAULT_DEBUG}}
 
 
 activate::qnet() {
-  local log_level="${1:-'QUIET'}"
-
-  if [[ ! -S "${QNET_MUX_SOCKET}" ]]; then
-    echo "Activating QNet jumphost with 2FA..."
-    ${SSH} -oLogLevel="${log_level}" "${QNET_JUMP_HOST}" "${CHECK_COMMAND}" || return 1
+  if [[ -S "${QNET_MUX_SOCKET}" ]]; then
+    echo "There's already a socket for QNet"
+    return 1
   fi
+
+  echo "Activating QNet jumphost with 2FA..."
+  ${SSH} -oLogLevel="${LOG_LEVEL}" "${QNET_JUMP_HOST}" "${CHECK_COMMAND}"
 }
 
 activate::openq() {
-  local log_level="${1:-'QUIET'}"
-
-  if [[ ! -S "${OPENQ_MUX_SOCKET}" ]]; then
-    echo "Activating OpenQ jumphost with 2FA..."
-    ${SSH} -oLogLevel="${log_level}" "${OPENQ_JUMP_HOST}" "${CHECK_COMMAND}" || return 1
+  if [[ -S "${OPENQ_MUX_SOCKET}" ]]; then
+    echo "There's already a socket for OpenQ"
+    return 2
   fi
+
+  echo "Activating OpenQ jumphost with 2FA..."
+  ${SSH} -oLogLevel="${LOG_LEVEL}" "${OPENQ_JUMP_HOST}" "${CHECK_COMMAND}"
 }
 
+setup() {
+  case ${DEBUG} in
+    true) LOG_LEVEL="DEBUG" ;;
+  esac
+}
 
 main() {
-  local log_level="QUIET"
-
-  case ${DEBUG} in
-    true) log_level="DEBUG" ;;
-  esac
-
-  activate::qnet ${log_level}
-  activate::openq ${log_level}
+  setup
+  activate::qnet  "${LOG_LEVEL}"
+  activate::openq "${LOG_LEVEL}"
 }
-
 
 #
 # ::main::
