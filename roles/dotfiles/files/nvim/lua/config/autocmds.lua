b368d7b116be139e3cca98c2011662eeef2ec11c
JJ <javier.juarez.martinez@ibm.com>
2026-03-03 16:44:47 +0100

    chore: Comment this until we have better templates

--- roles/dotfiles/files/nvim/lua/config/autocmds.lua
+++ roles/dotfiles/files/nvim/lua/config/autocmds.lua
@@ -6,31 +6,31 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd("BufNewFile", {
-  pattern = "*",
-  callback = function(args)
-    local ext = vim.fn.fnamemodify(args.file, ":e")
-    if ext == "" then
-      return
-    end
-
-    local template = template_dir .. "/templates" .. ext
-    if vim.api.nvim_buf_line_count(args.buf) ~= 1 then
-      return
-    end
-
-    local first = vim.api.nvim_bug_get_lines(args.buf, 0, 1, false)[1]
-    if first ~= "" then
-      return
-    end
-
-    -- Read the template
-    vim.cmd("silent 0read " .. vim.fn.fnameescape(template))
-
-    -- Remove the extra blank first line
-    vim.cmd("sinlent 1delete _")
-
-    -- Put cursor at end
-    vim.cmd("normal! G")
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufNewFile", {
+--   pattern = "*",
+--   callback = function(args)
+--     local ext = vim.fn.fnamemodify(args.file, ":e")
+--     if ext == "" then
+--       return
+--     end
+--
+--     local template = template_dir .. "/templates" .. ext
+--     if vim.api.nvim_buf_line_count(args.buf) ~= 1 then
+--       return
+--     end
+--
+--     local first = vim.api.nvim_bug_get_lines(args.buf, 0, 1, false)[1]
+--     if first ~= "" then
+--       return
+--     end
+--
+--     -- Read the template
+--     vim.cmd("silent 0read " .. vim.fn.fnameescape(template))
+--
+--     -- Remove the extra blank first line
+--     vim.cmd("sinlent 1delete _")
+--
+--     -- Put cursor at end
+--     vim.cmd("normal! G")
+--   end,
+-- })
