d819ad38cd052d2bfecf196ea7d7d591ba65173b
JJ <javier.juarez.martinez@ibm.com>
2026-09-08 17:35:01 +0200

    feat: Improvements for the docker retag utilities

--- roles/dotfiles/files/shell/zsh.d/docker.zsh
+++ roles/dotfiles/files/shell/zsh.d/docker.zsh
@@ -6,34 +6,41 @@ ibm::icr::login() {
   local -r account="${1}"
   local -r registry="${2}"
   local -rA IBMCLOUD_API_KEYS=(
-    qc-experimental "${QCEXPERIMENTAL_IBMCLOUD_API_KEY}"
-    qc-master       "${QCMASTER_IBMCLOUD_API_KEY}"
-    qs-staging      "${QSSTAGING_IBMCLOUD_API_KEY}"
-    qs-prod         "${QSPRODUCTION_IBMCLOUD_API_KEY}"
+    qc-master  "${QCMASTER_IBMCLOUD_API_KEY}"
+    qs-staging "${QSSTAGING_IBMCLOUD_API_KEY}"
+    qs-prod    "${QSPRODUCTION_IBMCLOUD_API_KEY}"
   )
   local -r api_key="${IBMCLOUD_API_KEYS[${account}]}"
 
-  if [[ -z "${api_key}" ]]; then
-    echo "Unknown account or missing API key: ${account}"
-    return 1
-  fi
+  [[ -n "${api_key}" ]] || return 1
 
   echo "${api_key}" | docker login --username iamapikey --password-stdin "${registry}"
 }
 
+
 docker::retag() {
-  local -r registry="icr.io"
-  local -r namespace="quantum-mirror-images"
   local -r image="${1}"
   local -r account="${2:-qc-master}"
-  local -r mirror_image="${registry}/${namespace}/${image}"
+  local -r registry="${3:-global}"
+  local -rA registries=(
+    us     "us.icr.io"
+    global "icr.io"
+    )
+  local -rA icr_namespaces=(
+    qc-master  "quantum-mirror-images"
+    qs-staging "qc-staging-ext-images"
+    qs-prod    "qc-production-ext-images"
+    )
+  local -r mirror_image="${registries[${registry}]}/${icr_namespaces[${account}]}/${image}"
 
   [[ -n "${image}" ]] || return 1
 
-  docker image pull --platform linux/amd64 "${image}"        || return 1
-  docker image tag "${image}" "${mirror_image}"              || return 1
-  ibm::icr::login "${account}" "${registry}"                 || return 1
-  docker image push --platform linux/amd64 "${mirror_image}"
+  docker image pull --platform linux/amd64 "${image}" || return 2
+  docker image tag "${image}" "${mirror_image}" || return 3
+
+  ibm::icr::login "${account}" "${registries[${registry}]}" || return 4
+
+  docker image push --platform linux/amd64 "${mirror_image}" || return 5
 }
 
 # autoloads
